1c01b689a1e9999b82cd8c2617ef0f187878635f
lpnt <vjc.ichi.149@gmail.com>
2021-12-07 22:35:33 +0700

    update

---
+++ week 3/exersdtt.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputheight;
+inputWidth = prompt( " enter the width");
+inputheight = prompt (' Enter the height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("the area is: " + area);
