3b7adefdacc3a0ec1e5ced7102bebd24d6b07000
unknown <vjc.ichi.149@gmail.com>
2021-12-29 11:40:08 +0700

    tubach

---
+++ README.md
@@ -0,0 +1,8 @@
+# 29/12/2021
+## Kết thúc module 31/12/2021
+- Cám ơn Anh/Chị đã quan tâm đến phần làm bài tập của em. 
+Các anh/chị muốn tìm đến file cần tìm kiếm thì chỉ cần dò theo tuần và bài, em có để theo tiếng anh hoặc đôi lúc là tiếng việt, vì do em đang học tiếng anh, nên mong a/chị thông cảm cho em ;) <3
+## Tự bạch
+- Gần kết thúc 1 năm bận rộn cũng là lúc gần kết thúc module 1... lòng có nhiều cảm xúc và mệt mỏi áp lực, vì đây là lần đầu tiếp xúc với công nghệ... Nhưng bản thân sẽ cố gắng tiếp tục và hoàn thành bản thân hơn
+- Xin gửi lời cám ơn đến tất cả anh em CodeGym, anh giám đốc trung tâm: Anh Khoa, các anh huấn luyện: anh cử, anh Minh, cùng gửi lời cám ơn đến đội ngũ.
+## 
--- week 3/mang/LTMang2/mang2.js
+++ week 3/mang/LTMang2/mang2.js
@@ -6,6 +6,7 @@ let a = [
     [3, 10, 26, 7]
 ];
 let arrayDefautl = ['a','b','c','1','2','n','t'];
+let arr = ['a','b','c',1,2,'n','t',4,8,0,10];
 
 function lesson1(){
     for (let i in a) {
@@ -22,13 +23,73 @@ function lesson2() {
 }
 
 function lesson3() {
-    let length = arrayDefautl.length;
     count = 0;
-    for (let i in arrayDefautl){
-        if(i == typeof String){
+    for (let i = 0; i < arr.length; i++){
+        if(typeof arr[i] == 'number'){
             
             count++;
         }
     }
-    document.write(count);
+    document.write(`in the array with numeric characters is : ${count}`);
+}
+
+function lesson4() {
+    count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if(typeof arr[i] == 'string'){
+            
+            count++;
+        }
+    }
+    document.write(`in the array with the number of words : ${count}`);
+
+}
+let str = `<div> 
+<input type="text" id="number1">Type Number<br>
+<input type="text" id="number2">Type Number<br>
+<button onclick="runLesson()">Run</button>
+</div>`;
+document.querySelector('#lesson5').addEventListener("click",runLesson5);
+function runLesson5() {
+    document.getElementById('lessonX').innerHTML = str;
+
+}
+function runLesson() {
+    let number1 = document.getElementById('number1').value;
+    let number2 = document.getElementById('number2').value;
+    let num1 = number1.split('');
+    let num2 = number2.split('');
+    for (let i = 0; i < num1.length; i++) {
+        if(num1[i] == num2[i]){
+            alert(`two array are the same`);
+            break;
+        } else {
+            alert(`two array are not the same`);
+            break;
+        }
+    }
+}
+
+let xb = `<div> 
+<input type="text" id="arrayX">Type Array<br>
+<button onclick="runLessonY()">Run</button>
+<p id="showArrayXY"></p>
+</div>`;
+document.querySelector('#lesson6').addEventListener("click",runLesson6);
+function runLesson6() {
+    document.getElementById('lessonY').innerHTML = xb;
+}
+
+function runLessonY() {
+    let text = document.getElementById('arrayX').value;
+    let arrText = text.split('');
+    let len = text.length;
+    for (let i = 0; i < len; i++ ) {
+        if (arrText[i] == '-') {
+            arrText.splice(i,1,'_')
+           
+            // return text;
+        }
+    }
+    document.getElementById('showArrayXY').innerHTML = arrText;
 }
