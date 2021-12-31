76e62010d49eee475a6bda740a7acc8ab3bff4b9
unknown <vjc.ichi.149@gmail.com>
2021-12-31 15:34:56 +0700

    update

--- MODULE 1/week 4/LapTrinhHuongDoiTuong/js/funnycircle.js
+++ MODULE 1/week 4/LapTrinhHuongDoiTuong/js/funnycircle.js
@@ -1,21 +1,22 @@
-// function Circle(x, y, radius) {
-//     this.x = x;
-//     this.y = y;
-//     this.radius = radius;
-// }
+function Circle(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
 
-//Vẽ 1 hình tròn kích thước cố định
+// Vẽ 1 hình tròn kích thước cố định
 
 // ;(function createCircle() {
 //     var ctx = document.getElementById('myCanvas').getContext('2d');
-//     var circle = new Circle(500, 500, 80);
+//     var circle = new Circle(500, 200, 80);
+//     var color = getRandomColor()
 //     ctx.beginPath();
 //     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+//     ctx.fillStyle = color;
 //     ctx.fill();
 // })();
 
 // vẽ đường tròn bán kính random;
-
 // function getRandomHex() {
 //     return Math.floor(Math.random() * 255);
 // }
@@ -24,30 +25,32 @@
 //     var red = getRandomHex();
 //     var green = getRandomHex();
 //     var blue = getRandomHex();
-//     return "rgb(" + red + "," + blue + "," + green +")";
-// }
-// ;(function createCircle() {
-//      var ctx = document.getElementById('myCanvas').getContext('2d');
-//      var radius = Math.floor(Math.random() * 80);
-//      var color = getRandomColor();
-//      var circle = new Circle(500, 500, radius);
-//      ctx.beginPath();
-//      ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
-//      ctx.fillStyle = color;
-//      ctx.fill();
-//  })();
-
-
-// function getRandomHex() {
-//     return Math.floor(Math.random() * 255);
+//     return `rgb(${red},${green},${blue})`
 // }
 
-// function getRandomColor() {
-//     var red = getRandomHex();
-//     var green = getRandomHex();
-//     var blue = getRandomHex;
-//         return "rbg(" + red + "," + blue + "," + green + ")";
-// }
+// createCircle();
+;(function createCircle() {
+     var ctx = document.getElementById('myCanvas').getContext('2d');
+     var radius = Math.floor(Math.random() * 80);
+     var color = getRandomColor();
+     var circle = new Circle(300, 300, radius);
+     ctx.beginPath();
+     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+     ctx.fillStyle = color;
+     ctx.fill();
+ })();
+
+
+function getRandomHex() {
+    return Math.floor(Math.random() * 255);
+}
+
+function getRandomColor() {
+    var red = getRandomHex();
+    var green = getRandomHex();
+    var blue = getRandomHex;
+        return "rbg(" + red + "," + blue + "," + green + ")";
+}
 
 // (function createCircle() {
 //     var ctx = document.getElementById('myCanvas').getContext('2d');
@@ -89,32 +92,32 @@
 //     ctx.fill();
 
 // }
-// createCircle();
 
-function Circle(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-}
 
-function getRandomHex() {
-    return Math.floor(Math.random() * 255);
-}
+// function Circle(x, y, radius) {
+//     this.x = x;
+//     this.y = y;
+//     this.radius = radius;
+// }
 
-function getRandomColor() {
-    var red = getRandomHex();
-    var green = getRandomHex();
-    var blue = getRandomHex();
-    return "rbg(" + red + ',' + blue + ',' + green + ")";
-}
+// function getRandomHex() {
+//     return Math.floor(Math.random() * 255);
+// }
 
-function createCircle() {
-    var ctx = document.getElementById('myCanvas').getContext('2d');
-    var radius = Math.floor(Math.random() * 80);
-    var color = getRandomColor();
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    var circle = new Circle(x, y, radius);
-    ctx.beginPath();
-    ctx.arc()
-}
+// function getRandomColor() {
+//     var red = getRandomHex();
+//     var green = getRandomHex();
+//     var blue = getRandomHex();
+//     return "rbg(" + red + ',' + blue + ',' + green + ")";
+// }
+
+// function createCircle() {
+//     var ctx = document.getElementById('myCanvas').getContext('2d');
+//     var radius = Math.floor(Math.random() * 80);
+//     var color = getRandomColor();
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     var circle = new Circle(x, y, radius);
+//     ctx.beginPath();
+//     ctx.arc()
+// }
